1e0a15f08801fe5f0f5b694b0edb85e48126d9e6
freshnewkicks <tylerdukedev@gmail.com>
2022-01-02 12:56:13 -0500

    Added GET root path method

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ const app = express();
 app.listen(3000, () => {
     console.log(`Listening on Port 3000`)
 });
+
+app.get('/', (req,res) => {
+    res.send('Express app on Heroku is operational..')
+})
