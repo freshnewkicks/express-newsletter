849d7c8d918338e46631355a126414e4b9ce172e
freshnewkicks <tylerdukedev@gmail.com>
2022-01-03 15:17:55 -0500

    Fixed 404 and success responses

--- server.js
+++ server.js
@@ -17,11 +17,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname)); // static method will use the current directory
                                     // to serve static files
 
-mailchimp.setConfig({
-    apiKey: "a54125c1484791914ba3ba429d155840-us20",
-    server: "us20.admin.mailchimp.com/"
-});
-
 app.get('/',  (req,res) => {
     res.sendFile('/index.html');
 });
@@ -30,7 +25,7 @@ app.post('/success', (req,res) => {
     const formRequestBody = req.body;
     const listId = '9fe9636f6f';
 
-    let data = {
+    let membersData = {
         members: [
             {
                 email_address: formRequestBody.email,
@@ -44,18 +39,35 @@ app.post('/success', (req,res) => {
         ]
     };
 
-    const jsonData = JSON.stringify(data);
+    // turns data
+    const jsonData = JSON.stringify(membersData);
     const url = `https://us20.api.mailchimp.com/3.0/lists/${listId}`;
     const options = {
         method: "POST",
-        auth: `${data.members[0].merge_fields.FNAME}:a54125c1484791914ba3ba429d155840-us20`
+        auth: `${membersData.members[0].merge_fields.FNAME}:a54125c1484791914ba3ba429d155840-us20`
     }
+
+    // we want to post data to external resource
+    // instead of getting data from an external server
+    // https.request will make a request to post data to an external server
+
     const request = https.request(url, options, function(response){
-        response.on('data', (data) => {
-            console.log(JSON.parse(data));
-        })
+        if (response.statusCode === 200) {
+            response.on('data', (data) => {
+                console.log(JSON.parse(data));
+            });
+            res.sendFile(__dirname + '/success.html');
+        } else if (response.statusCode !== 200) {
+            response.on('data', (data) => {
+                console.log(JSON.parse(data));
+                res.sendFile(__dirname + '/failure.html');
+            })
+        }
+
     })
 
+    // call methods on the https request method
+    //
     request.write(jsonData);
     request.end();
 });
